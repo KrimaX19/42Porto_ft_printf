eada740034298e6d0e11172a90d2094814eb7b17
Rui Miguel Soares Martins <rusoares@c1r3s7.42porto.com>
2024-04-20 00:09:07 +0100

    mm

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/26 16:48:26 by rusoares          #+#    #+#              #
-#    Updated: 2023/11/26 16:48:26 by rusoares         ###   ########.fr        #
+#    Updated: 2024/04/19 22:52:43 by rusoares         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,18 +17,17 @@ OBJS = $(SRCS:.c=.o)
 #BONUS = 
 #BOBJS = $(BONUS:.c=.o)
 
-CC = cc
-FLAGS = -Wall -Wextra -Werror -c -I.
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -c 
 NAME = libftprintf.a
 
 all: $(NAME)
 
 %.o: %.c
-	$(CC) $(FLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) $< -o $@
 
 $(NAME): $(OBJS)
-	$(CC) $(FLAGS) -o $(NAME) $(OBJS)
-	ar rcs $(NAME) $(OBJS)
+	ar -r $(NAME) $(OBJS)
 
 clean:
 	rm -f $(OBJS) 
@@ -37,7 +36,8 @@ fclean: clean
 	rm -f $(NAME)
 
 re: fclean all
+#so:
+#	$(CC) -nostartfiles -fPIC $(CFLAGS) $(SRCS)
+#	cc -nostartfiles -shared -o libft.so $(OBJS)
 
 #bonus:
-
-PHONY: all bonus clean fclean re
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:15:03 by rusoares          #+#    #+#             */
-/*   Updated: 2024/02/25 18:32:56 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/04/19 23:57:52 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,11 @@
 #include <stdarg.h>
 //#include "../42Porto_ft_printf/libft/src"
 
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-
-int	test(int c)
-{
-	write (1, &c, 1);
-	return (1);
-}
-
-int	testd(int d)
-{
-	ft_putnbr_fd (d, 1);
-	return (2);
-}
-
-int	tests(char *s)
-{
-	ft_putstr_fd (s, 1);
-	return (3);
-}
-/*
-int	test_x (unsigned int x)
-{
-	ft_putnbr_fd (x, 0, 0, 0);
-	return (4);
-}
-*/
-
-int	testx(unsigned int x)
-{
-	ft_putnbr_fd (x, 0);
-	return (4);
-}
+int	ft_putnbr_fd(int n, int fd);
+int	ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int	ft_print_hex(size_t nb, int flag);
+int	countnb(int nb);
 
 int	ft_printf(const char *str, ...)
 {
@@ -59,151 +30,110 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 	length = 0;
 	va_start(ptr, str);
-
 	while (str[i])
 	{
-		while (str[i] == '%')
+		if (str[i] == '%' && ++i)
 		{
-			i++;
-			if (str[i] == '+')
-			{
-				i++;
-			}
-			if (str[i] == ' ')
-			{
-				i++;
-			}
-			if (str[i] == 'c')
-			{
-				length = test(va_arg(ptr, int));
-			}
-			if (str[i] == '%')
-			{
-				write (1, "%", 1);
-			}
-			if (str[i] == 'd')
-			{
-				length = testd(va_arg(ptr, int));
-			}
-			if (str[i] == 's')
-			{
-				length = tests(va_arg(ptr, char *));
-			}
-			if (str[i] == 'x')
-			{
-				length = testx(va_arg(ptr, unsigned int));
-			}
-			i++;
+			if (str[i] == 'c')//ok
+				length += ft_putchar_fd(va_arg(ptr, int), 1);
+			if (str[i] == '%')//ok
+				length += write (1, "%", 1);
+			if (str[i] == 'd' || str[i] == 'i' || str[i] =='u')//ok (teste u needed)
+				length += countnb(va_arg(ptr, int));
+			if (str[i] == 's')//ok
+				length += ft_putstr_fd(va_arg(ptr, char *), 1);
+			if (str[i] == 'x' || str[i] == 'X' || str[i] == 'p')//ok
+				length += ft_print_hex(va_arg(ptr, unsigned int), str[i]);
 		}
-		write(1, &str[i], 1);
+		else
+			length += write(1, &str[i], 1);
 		i++;
 	}
+	va_end(ptr);
 	return (length);
 }
 
-int	main(void)
-{
-	printf("c\n");
-    ft_printf("c\n");
-	ft_printf("%c\n", '0');
-	printf("%c\n", '0');
-	ft_printf(" %c \n", '0');
-	printf(" %c \n", '0');
-	ft_printf(" %c\n", '0' - 256);
-	printf(" %c\n", '0' - 256);
-	ft_printf("%c \n", '0' + 256);
-	printf("%c \n", '0' + 256);
-	ft_printf(" %c %c %c \n", '0', 0, '1');
-	printf(" %c %c %c \n", '0', 0, '1');
-	ft_printf(" %c %c %c \n", ' ', ' ', ' ');
-	printf(" %c %c %c \n", ' ', ' ', ' ');
-	ft_printf(" %c %c %c \n", '1', '2', '3');
-	printf(" %c %c %c \n", '1', '2', '3');
-	ft_printf(" %c %c %c \n", '2', '1', 0);
-	printf(" %c %c %c \n", '2', '1', 0);
-	ft_printf(" %c %c %c \n", 0, '1', '2');
-	printf(" %c %c %c \n", 0, '1', '2');
-    ft_printf("%%\n");
-	printf("%%\n");
-	ft_printf("FT_PRINTF: %%:\n");
-	printf("   PRINTF: %%:\n");
-	ft_printf("FT_PRINTF: %%%%:\n");
-	printf("   PRINTF: %%%%:\n");
-	ft_printf("FT_PRINTF: %% %% %%:\n");
-	printf("   PRINTF: %% %% %%:\n");
-	printf("d\n");
-	ft_printf("FT_PRINTF: :%d:\n", 0);
-	printf("   PRINTF: :%d:\n", 0);
-	ft_printf("FT_PRINTF: :%d:\n",-1);
-	printf("   PRINTF: %d\n",-1);
-	ft_printf(": %d :\n", 100);
-	printf(": %d :\n", 100);
-	printf("s\n");
-	ft_printf("FT_PRINTF: %s:\n", "Teste string");
-	printf("   PRINTF: %s:\n", "Teste string");
-	printf("X\n");
-	ft_printf("FT_PRINTF: X %X:\n", 42);
-	printf("   PRINTF: X %X:\n", 42);
-	printf("x\n");
-	ft_printf("FT_PRINTF: x %x:\n", 42);
-	printf("   PRINTF: x %x:\n", 42);
-	return (0);
-}
+//int	main(void)
+//{
+//	int x = ft_printf("%d", 0);
+//	printf("%d", x);
+//	return (0);
+//}
 
-
-
-void	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(int n, int fd)
 {
 	unsigned long	nb;
+	int		ret;
 
 	nb = n;
+	ret = 0;
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
+		ret += ft_putchar_fd('-', fd);
 		nb = -1 * nb;
 	}
 	if (nb >= 10)
 	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putnbr_fd(nb % 10, fd);
+		ret += ft_putnbr_fd(nb / 10, fd);
+		ret += ft_putnbr_fd(nb % 10, fd);
 	}
 	else
 	{
-		ft_putchar_fd(nb + '0', fd);
+		ret += ft_putchar_fd(nb + '0', fd);
 	}
+	return (ret);
 }
 
 int	countnb(int nb)
 {
-	int	i;
-	int	n;
+	return (ft_putnbr_fd(nb, 1));
+}
+
+int	ft_putchar_fd(char c, int fd)
+{
+	size_t	i;
 
 	i = 0;
-	n = nb;
-	while (n)
+	if (fd > 0)
 	{
-		n = n / 10;
-		i++;
+		i += write(1, &c, 1);
 	}
 	return (i);
 }
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
-	if (fd > 0)
-	{
-		write(fd, &c, 1);
-	}
-}
+	size_t	i;
 
-void	ft_putstr_fd(char *s, int fd)
-{
+	i = 0;
 	if (fd >= 0)
 	{
 		while (*s)
 		{
-			ft_putchar_fd(*s, fd);
+			i += write (1, s, 1);
 			s++;
+			
 		}
 	}
+	return (i);
+}
+int	ft_print_hex(size_t nb, int flag)
+{
+	char	*str;
+	int 	i;
+
+	i = 0;
+	if (flag == 'x' || flag == 'p')
+		str = "0123456789abcdef";
+	if (flag == 'X')
+		str = "0123456789ABCDEF";
+	if (flag == 'p')
+	{
+		i = write(1,"0x",2);
+		flag = 'x';
+	}
+	if (nb >= 16)
+		ft_print_hex(nb / 16, flag);
+	return (i + ft_putchar_fd(str[nb % 16], 1));
+	
 }
