faa00d2a5e27cc592493e38b9b94995b84c650c1
Rui Miguel Soares Martins <rusoares@c2r5s7.42porto.com>
2024-04-30 22:43:54 +0100

    a funcionar

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:15:03 by rusoares          #+#    #+#             */
-/*   Updated: 2024/04/19 23:57:52 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/04/30 22:43:26 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 #include <stdarg.h>
 //#include "../42Porto_ft_printf/libft/src"
 
-int	ft_putnbr_fd(int n, int fd);
+int	ft_putnbr_fd(long int n, int fd);
 int	ft_putchar_fd(char c, int fd);
-int		ft_putstr_fd(char *s, int fd);
-int	ft_print_hex(size_t nb, int flag);
-int	countnb(int nb);
+int	ft_putstr_fd(char *s);
+int	ft_print_hex(long long int nb, int flag);
+int	ft_putnbr_u(unsigned int n, int fd);
+int	ft_print_p(unsigned long long int nb, int flag);
 
 int	ft_printf(const char *str, ...)
 {
@@ -34,16 +35,20 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%' && ++i)
 		{
-			if (str[i] == 'c')//ok
+			if (str[i] == 'c')//OK
 				length += ft_putchar_fd(va_arg(ptr, int), 1);
-			if (str[i] == '%')//ok
-				length += write (1, "%", 1);
-			if (str[i] == 'd' || str[i] == 'i' || str[i] =='u')//ok (teste u needed)
-				length += countnb(va_arg(ptr, int));
-			if (str[i] == 's')//ok
-				length += ft_putstr_fd(va_arg(ptr, char *), 1);
-			if (str[i] == 'x' || str[i] == 'X' || str[i] == 'p')//ok
+			if (str[i] == '%')//OK
+				length += write (1, "%", 1);//OK
+			if (str[i] == 'd' || str[i] == 'i')
+				length += ft_putnbr_fd(va_arg(ptr, int), 1);//OK
+			if (str[i] == 'u')
+				length += ft_putnbr_u(va_arg(ptr, int), 1);//OK
+			if (str[i] == 's')//NOK
+				length += ft_putstr_fd(va_arg(ptr, char *));
+			if (str[i] == 'x' || str[i] == 'X')//xX = OK
 				length += ft_print_hex(va_arg(ptr, unsigned int), str[i]);
+			if (str[i] == 'p') //P = OK
+				length += ft_print_p(va_arg(ptr, long int), str[i]);
 		}
 		else
 			length += write(1, &str[i], 1);
@@ -53,14 +58,7 @@ int	ft_printf(const char *str, ...)
 	return (length);
 }
 
-//int	main(void)
-//{
-//	int x = ft_printf("%d", 0);
-//	printf("%d", x);
-//	return (0);
-//}
-
-int	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(long int n, int fd)
 {
 	unsigned long	nb;
 	int		ret;
@@ -78,15 +76,8 @@ int	ft_putnbr_fd(int n, int fd)
 		ret += ft_putnbr_fd(nb % 10, fd);
 	}
 	else
-	{
 		ret += ft_putchar_fd(nb + '0', fd);
-	}
 	return (ret);
-}
-
-int	countnb(int nb)
-{
-	return (ft_putnbr_fd(nb, 1));
 }
 
 int	ft_putchar_fd(char c, int fd)
@@ -95,29 +86,26 @@ int	ft_putchar_fd(char c, int fd)
 
 	i = 0;
 	if (fd > 0)
-	{
 		i += write(1, &c, 1);
-	}
 	return (i);
 }
 
-int	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s)
 {
 	size_t	i;
 
 	i = 0;
-	if (fd >= 0)
+
+	if (s == NULL)
+		return(i + write (1, "(null)", 6));
+	while (*s)
 	{
-		while (*s)
-		{
-			i += write (1, s, 1);
-			s++;
-			
-		}
+	i += write (1, s, 1);
+	s++;
 	}
 	return (i);
 }
-int	ft_print_hex(size_t nb, int flag)
+int	ft_print_hex(long long int nb, int flag)
 {
 	char	*str;
 	int 	i;
@@ -129,11 +117,44 @@ int	ft_print_hex(size_t nb, int flag)
 		str = "0123456789ABCDEF";
 	if (flag == 'p')
 	{
-		i = write(1,"0x",2);
+		i += write(1,"0x",2);
 		flag = 'x';
 	}
 	if (nb >= 16)
-		ft_print_hex(nb / 16, flag);
+		i += ft_print_hex(nb / 16, flag);
 	return (i + ft_putchar_fd(str[nb % 16], 1));
-	
+}
+int	ft_putnbr_u(unsigned int n, int fd)
+{
+	unsigned long	nb;
+	int		ret;
+
+	nb = n;
+	ret = 0;
+	if (nb >= 10)
+	{
+		ret += ft_putnbr_fd(nb / 10, fd);
+		ret += ft_putnbr_fd(nb % 10, fd);
+	}
+	else
+		ret += ft_putchar_fd(nb + '0', fd);
+	return (ret);
+}
+int	ft_print_p(unsigned long long int nb, int flag)
+{
+	char	*str;
+	unsigned long long int  	i;
+	i = 0;
+	if (flag == 'x' || flag == 'p')
+		str = "0123456789abcdef";
+	if (nb == 0)
+		return(i + write(1, "(nil)", 5));
+	if (flag == 'p')
+	{
+		i += write(1,"0x",2);
+		flag = 'x';
+	}
+	if (nb >= 16)
+		i += ft_print_hex(nb / 16, flag);
+	return (i + ft_putchar_fd(str[nb % 16], 1));
 }
--- research.md
+++
@@ -1,6 +0,0 @@
-1 - https://www.tutorialspoint.com/c_standard_library/c_function_printf.htm
-2 - https://cplusplus.com/reference/cstdio/printf/
-3 - https://en.wikipedia.org/wiki/Printf
-4 - https://www.educative.io/answers/how-to-use-printf-in-c
-5 - https://csnotes.medium.com/ft-printf-tutorial-42project-f09b6dc1cd0e
-6 - 
